006663e06da8caf513ac635ec2e91bf2861d4855
EKVIP\tdie <t.diebelt@ekvip.de>
2023-12-06 06:21:08 +0100

    cleanup day 6

--- src/Solutions/Day06/SolverDay06.cs
+++ src/Solutions/Day06/SolverDay06.cs
@@ -22,34 +22,21 @@ namespace Solutions.Day06
     {
         public long SolvePart1(string[] lines)
         {
-            //Parse
-            // 0,3 -> 1,4
-            // 2,1 -> 4,3
-            //var rocklines = lines.Select(x => x.Split("->").ToList().Select(y => (Int32.Parse(y.Trim().Split(',')[0]), Int32.Parse(y.Trim().Split(',')[1]))).ToList());
-
-            //1,3,5,1,2,455,6
-            //var longs = lines[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt64(x));
-
-            //1
-            //3
-            //var sum = lines.Select(y => Convert.ToInt64(y)).Map(x => x).Reduce((x, y) => x + y);
-
             long result = 1;
-            var array = new int[lines.Length, lines[0].Length];
 
             var times = lines[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(y => Convert.ToInt64(y)).ToList();
             var distances = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(y => Convert.ToInt64(y)).ToList();
 
             for (int i = 0; i < times.Count(); i++)
             {
-                long tmp = 0;
+                long wins = 0;
                 for (long j = 0; j < times[i]; j++)
                 {
                     long speed = j;
                     long travel = (j * (times[i] - j));
-                    if (travel > distances[i]) tmp++;
+                    if (travel > distances[i]) wins++;
                 }
-                result *= tmp;
+                result *= wins;
             }
 
             //Solve
@@ -58,20 +45,7 @@ namespace Solutions.Day06
 
         public long SolvePart2(string[] lines)
         {
-            //Parse
-            // 0,3 -> 1,4
-            // 2,1 -> 4,3
-            //var rocklines = lines.Select(x => x.Split("->").ToList().Select(y => (Int32.Parse(y.Trim().Split(',')[0]), Int32.Parse(y.Trim().Split(',')[1]))).ToList());
-
-            //1,3,5,1,2,455,6
-            //var longs = lines[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt64(x));
-
-            //1
-            //3
-            //var sum = lines.Select(y => Convert.ToInt64(y)).Map(x => x).Reduce((x, y) => x + y);
-
             long result = 0;
-            var array = new int[lines.Length, lines[0].Length];
 
             var times = lines[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(y => Convert.ToInt64(y)).ToList();
             var distances = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(y => Convert.ToInt64(y)).ToList();
