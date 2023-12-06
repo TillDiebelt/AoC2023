478f41a28f9bbfa88e818b77b5e626eda9c7e27d
EKVIP\tdie <t.diebelt@ekvip.de>
2023-12-06 08:39:02 +0100

    fancy day 6 in "linear" time, still small bug that I am to lazy to fix

--- src/Solutions/Day06/SolverDay06.cs
+++ src/Solutions/Day06/SolverDay06.cs
@@ -15,13 +15,14 @@ using System.Security.Cryptography.X509Certificates;
 using System.Reflection.Emit;
 using System.Text.Json;
 using AOCLib;
+using GIGACHADINT = System.Numerics.BigInteger;
 
 namespace Solutions.Day06
 {
     public class SolverDay06 : ISolver
     {
         public long SolvePart1(string[] lines)
-        {
+        {            
             long result = 1;
 
             var times = lines[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(y => Convert.ToInt64(y)).ToList();
@@ -49,17 +50,26 @@ namespace Solutions.Day06
 
             var times = lines[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(y => Convert.ToInt64(y)).ToList();
             var distances = lines[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(y => Convert.ToInt64(y)).ToList();
-            long time = long.Parse(times.Aggregate("", (string x, long y) => x + "" + y));
-            long dist = long.Parse(distances.Aggregate("", (string x, long y) => x + "" + y));
+            long time = MergeNums(times);
+            long dist = MergeNums(distances);
 
-            for (long j = 0; j < time; j++)
-            {
-                long speed = j;
-                long travel = (j * (time - j));
-                if (travel > dist) result++;
-            }
+            //could be off by 1, idc
+            result = (long)Math.Ceiling(Math.Sqrt(Math.Pow(time, 2) - 4 * dist));
 
             //Solve
+            return result;
+        }
+
+        private long MergeNums(List<long> nums) 
+        {
+            nums.Reverse();
+            long result = nums.First();
+            long mult = 1;
+            for (int i = 1; i < nums.Count; i++)
+            {
+                while (result > mult) mult *= 10;
+                result += nums[i]*mult;
+            }
             return result;
         }
     }
