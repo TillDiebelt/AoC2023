61a9ef9b9af036c5cfbd51bc0e00a3ff7e703e3a
Till Diebelt <till.diebelt@stud.htwk-leipzig.de>
2023-12-14 07:26:33 +0100

    solutions day 14

--- src/Solutions/Day14/SolverDay14.cs
+++ src/Solutions/Day14/SolverDay14.cs
@@ -36,53 +36,202 @@ namespace Solutions.Day14
     {
         public long SolvePart1(string[] lines, string text)
         {
-            //Parse
-            // 0,3 -> 1,4
-            // 2,1 -> 4,3
-            //var rocklines = lines.Select(x => x.Split("->").ToList().Select(y => (Int32.Parse(y.Trim().Split(',')[0]), Int32.Parse(y.Trim().Split(',')[1]))).ToList());
-
-            //1,3,5,1,2,455,6
-            //var longs = lines[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt64(x));
-
-            List<long> longs = new();
+            for (int i = 0; i < lines.Length; i++)
+                lines[i] = lines[i].Trim();
+            int[,] map = new int[lines.Length, lines[0].Length];
             for(int i = 0; i < lines.Length; i++)
             {
-                //var splits = lines[i].Split(":")[1].Trim().Split(",").Select(x => Int64.Parse(x)).ToList();
                 for (int j = 0; j < lines[i].Length; j++)
                 {
-                    
+                    switch (lines[i][j])
+                    {
+                        case 'O':
+                            map[i, j] = 2;
+                            break;
+                        case '#':
+                            map[i, j] = 1;
+                            break;
+                    }
                 }
             }
 
-            for (int i = 0; i < longs.Count; i++)
+
+            for (int i = 1; i < lines.Length; i++)
             {
-
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (map[i,j] == 2)
+                    {
+                        map[i, j] = 0;
+                        int y = i;
+                        while(y >= 0 && map[y, j] == 0)
+                        {
+                            y--;
+                        }
+                        y++;
+                        map[y, j] = 2;
+                    }
+                }
             }
-
-
+            
             //Solve
             long result = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (map[i, j] == 2)
+                    {
+                        result += lines.Length - i;
+                    }
+                }
+            }
+            
             return result;
         }
 
         public long SolvePart2(string[] lines, string text)
         {
-            //Parse
-            // 0,3 -> 1,4
-            // 2,1 -> 4,3
-            //var rocklines = lines.Select(x => x.Split("->").ToList().Select(y => (Int32.Parse(y.Trim().Split(',')[0]), Int32.Parse(y.Trim().Split(',')[1]))).ToList());
+            for (int i = 0; i < lines.Length; i++)
+                lines[i] = lines[i].Trim();
+            int[,] map = new int[lines.Length, lines[0].Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    switch (lines[i][j])
+                    {
+                        case 'O':
+                            map[i, j] = 2;
+                            break;
+                        case '#':
+                            map[i, j] = 1;
+                            break;
+                    }
+                }
+            }
 
-            //1,3,5,1,2,455,6
-            //var longs = lines[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt64(x));
 
-            //1
-            //3
-            //var sum = lines.Select(y => Convert.ToInt64(y)).Map(x => x).Reduce((x, y) => x + y);
-
+            List<(string,int)> seen = new List<(string, int)>();
+            bool finalLoops = false;
+            int loops = 1000;
+            string key = MapToString(map);
+            seen.Add((key,0));
+            for (int r = 1; r <= loops; r++)
+            {
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    for (int j = 0; j < lines[i].Length; j++)
+                    {
+                        if (map[i, j] == 2)
+                        {
+                            map[i, j] = 0;
+                            int y = i;
+                            while (y >= 0 && map[y, j] == 0)
+                            {
+                                y--;
+                            }
+                            y++;
+                            map[y, j] = 2;
+                        }
+                    }
+                }
+                for (int i = 1; i < lines[0].Length; i++)
+                {
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+                        if (map[j, i] == 2)
+                        {
+                            map[j, i] = 0;
+                            int x = i;
+                            while (x >= 0 && map[j, x] == 0)
+                            {
+                                x--;
+                            }
+                            x++;
+                            map[j,x] = 2;
+                        }
+                    }
+                }
+                for (int i = lines.Length - 2; i >= 0; i--)
+                {
+                    for (int j = 0; j < lines[0].Length; j++)
+                    {
+                        if (map[i, j] == 2)
+                        {
+                            map[i, j] = 0;
+                            int y = i;
+                            while (y < lines.Length && map[y, j] == 0)
+                            {
+                                y++;
+                            }
+                            y--;
+                            map[y, j] = 2;
+                        }
+                    }
+                }
+                for (int i = lines[0].Length - 2; i >= 0; i--)
+                {
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+                        if (map[j, i] == 2)
+                        {
+                            map[j, i] = 0;
+                            int x = i;
+                            while (x < lines[0].Length && map[j, x] == 0)
+                            {
+                                x++;
+                            }
+                            x--;
+                            map[j, x] = 2;
+                        }
+                    }
+                }
+                if (!finalLoops)
+                {
+                    key = MapToString(map);
+                    if (seen.Any(x => x.Item1 == key))
+                    {
+                        int foo = seen.Where(x => x.Item1 == key).First().Item2;
+                        int foo2 = r- foo;
+                        while (foo + foo2 <= loops)
+                            foo += foo2;
+                        r = foo;
+                        finalLoops = true;
+                    }
+                    else
+                        seen.Add((key,r));
+                }
+            }
 
             //Solve
-            long result = Utils.GaussSum(10, 5);
+            long result = 0;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                for (int j = 0; j < lines[i].Length; j++)
+                {
+                    if (map[i, j] == 2)
+                    {
+                        result += lines.Length - i;
+                    }
+                }
+            }
+
             return result;
+        }
+
+        private string MapToString(int[,] m)
+        {
+            string res = "";
+
+            for (int i = 0; i <= m.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= m.GetUpperBound(1); j++)
+                {
+                    res += ""+m[i,j];
+                }
+            }
+            return res;
         }
     }
 }
